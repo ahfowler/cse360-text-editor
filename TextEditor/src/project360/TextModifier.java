5e638fcb322b9a0f0ffbcf8923bbe05312092bfc
ahfowler <fowler.azaria@gmail.com>
2019-12-02 13:26:19 -0700

    Added basic format for TextModifier

--- TextEditor/src/project360/TextModifier.java
+++ TextEditor/src/project360/TextModifier.java
@@ -2,5 +2,21 @@ package project360;
 
 public class TextModifier 
 {
-
+	private String rawText;
+	private String formattedString;
+	
+	
+	TextModifier (String text) {
+		rawText = text;
+	}
+	
+	// Methods based on command.
+	
+	void formatString(String text) {
+		// Logic on parsing the rawText using the methods above.
+	}
+	
+	String getFormattedString() {
+		return formattedString;
+	}
 }
