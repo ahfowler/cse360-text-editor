98dc33b757b9f256ee06c263d1985b87159a1991
tcgordo1 <tcgordo1@users.noreply.github.com>
2019-12-04 21:48:00 -0700

    Indentation added

--- TextEditor/src/project360/Gui.java
+++ TextEditor/src/project360/Gui.java
@@ -17,57 +17,51 @@ package project360;
  *  @version 1.0 November 22, 2019
  **/
 
-import java.awt.*; 
+import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
-import java.io.*; 
-import java.awt.event.*; 
-import javax.swing.plaf.metal.*; 
-import javax.swing.text.*; 
+import java.io.*;
+import java.awt.event.*;
+import javax.swing.plaf.metal.*;
+import javax.swing.text.*;
 
 /**
- * Creates a GUI object for the 360 Text Editor application 
+ * Creates a GUI object for the 360 Text Editor application
  */
-class Gui extends JFrame implements ActionListener 
-{ 
-	static String textStr = ""; //hold user text
-	static boolean imported = false; //true if a file has been imported 
+class Gui extends JFrame implements ActionListener {
+	static String textStr = ""; // hold user text
+	static boolean imported = false; // true if a file has been imported
 
-	// Frame Object
-	JFrame frameOne; 
+	// Frame Objecta
+	JFrame frameOne;
 
-	// Text Viewer Objects 
-	JTextArea topText; 		//input viewer
-	JTextArea bottomText;   //output viewer 
+	// Text Viewer Objects
+	JTextArea topText; // input viewer
+	JTextArea bottomText; // output viewer
 
 	/**
-	 * Constructor for the Gui class 
+	 * Constructor for the Gui class
 	 */
-	Gui() 
-	{ 
-		//create frame
-		frameOne = new JFrame("360 Text Editor"); 
-		//exits program when window is closed
+	Gui() {
+		// create frame
+		frameOne = new JFrame("360 Text Editor");
+		// exits program when window is closed
 		frameOne.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
 		JPanel panel = new JPanel();
 		panel.setBorder(new EmptyBorder(10, 10, 10, 10));
-		panel.setLayout(new GridLayout(2,1,5,5));
+		panel.setLayout(new GridLayout(2, 1, 5, 5));
 
+		// set GUI theme
+		try {
+			UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+			MetalLookAndFeel.setCurrentTheme(new OceanTheme());
+		} catch (Exception e) {
+		}
 
-		//set GUI theme
-		try 
-		{ 
-			UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel"); 
-			MetalLookAndFeel.setCurrentTheme(new OceanTheme()); 
-		} 
-		catch (Exception e) 
-		{ 
-		} 
-
-		// Create text boxes 
-		topText = new JTextArea(); 
+		// Create text boxes
+		topText = new JTextArea();
 		bottomText = new JTextArea();
 
 		topText.setLineWrap(true);
@@ -76,23 +70,21 @@ class Gui extends JFrame implements ActionListener
 		topText.setEditable(false);
 
 		JScrollPane scroll = new JScrollPane(topText);
-	
+
 		bottomText.setLineWrap(true);
 		bottomText.setWrapStyleWord(true);
-		bottomText.setMargin(new Insets(55,55,55,55));
+		bottomText.setMargin(new Insets(55, 55, 55, 55));
 		bottomText.setEditable(false);
 		bottomText.setFont(new Font("monospaced", Font.PLAIN, 10));
 
 		JScrollPane scrollb = new JScrollPane(bottomText);
 
-		scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.
-				VERTICAL_SCROLLBAR_ALWAYS);
-		scrollb.setVerticalScrollBarPolicy(ScrollPaneConstants.
-				VERTICAL_SCROLLBAR_ALWAYS);
+		scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollb.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 
 		JTextPane panelOne = new JTextPane();
 		StyledDocument doc = panelOne.getStyledDocument();
-		
+
 		JTextPane panelTwo = new JTextPane();
 		StyledDocument doc2 = panelOne.getStyledDocument();
 
@@ -105,185 +97,154 @@ class Gui extends JFrame implements ActionListener
 		SimpleAttributeSet left = new SimpleAttributeSet();
 		StyleConstants.setAlignment(left, StyleConstants.ALIGN_LEFT);
 
-		//		try
-		//		{
-		//			
-		//		}
-		//		catch(Exception e) 
-		//		{
-		//			
-		//		}
+		// try
+		// {
+		//
+		// }
+		// catch(Exception e)
+		// {
+		//
+		// }
 
-		panelOne.setMargin(new Insets(10,10,10,10));
-		//p.setBounds(10, 320, 580, 300);
+		panelOne.setMargin(new Insets(10, 10, 10, 10));
+		// p.setBounds(10, 320, 580, 300);
 
-		// Create a menu bar 
+		// Create a menu bar
 		JMenuBar menuBar = new JMenuBar();
 
-		// Create a menu bar options option 
-		JMenu fileOpt = new JMenu("File"); 
-		JMenu runOpt = new JMenu("Run"); 
-		JMenu helpOpt = new JMenu("Help"); 
+		// Create a menu bar options option
+		JMenu fileOpt = new JMenu("File");
+		JMenu runOpt = new JMenu("Run");
+		JMenu helpOpt = new JMenu("Help");
 
-		menuBar.add(fileOpt); 
-		menuBar.add(runOpt); 
-		menuBar.add(helpOpt); 
+		menuBar.add(fileOpt);
+		menuBar.add(runOpt);
+		menuBar.add(helpOpt);
 
-		// Create menu items 
-		JMenuItem fImport = new JMenuItem("Import"); 
-		JMenuItem fExport = new JMenuItem("Export"); 
+		// Create menu items
+		JMenuItem fImport = new JMenuItem("Import");
+		JMenuItem fExport = new JMenuItem("Export");
 		JMenuItem rClear = new JMenuItem("Clear");
 		JMenuItem rProcess = new JMenuItem("Process");
 		JMenuItem hHelp = new JMenuItem("Flag Guide");
 
-		fImport.addActionListener(this); 
-		fExport.addActionListener(this); 
-		rClear.addActionListener(this); 
-		rProcess.addActionListener(this); 
-		hHelp.addActionListener(this); 
+		fImport.addActionListener(this);
+		fExport.addActionListener(this);
+		rClear.addActionListener(this);
+		rProcess.addActionListener(this);
+		hHelp.addActionListener(this);
 
-		fileOpt.add(fImport); 
-		fileOpt.add(fExport); 
+		fileOpt.add(fImport);
+		fileOpt.add(fExport);
 		runOpt.add(rClear);
 		runOpt.add(rProcess);
 		helpOpt.add(hHelp);
 
-
-		// Controls and sets frame formatting 
+		// Controls and sets frame formatting
 		frameOne.setJMenuBar(menuBar);
 
 		panel.add(scroll);
-		//panel.add(panelOne);
+		// panel.add(panelOne);
 
 		panel.add(scrollb);
-		//panel.add(panelTwo);
-		
-		frameOne.add(panel);
+		// panel.add(panelTwo);
 
+		frameOne.add(panel);
 
 		frameOne.setSize(635, 700);
 		frameOne.setLocationRelativeTo(null);
 
 		frameOne.show();
 
-		JOptionPane.showMessageDialog(frameOne, "Hello!\n"
-				+ "Please import a valid text file to get started.\n"
+		JOptionPane.showMessageDialog(frameOne, "Hello!\n" + "Please import a valid text file to get started.\n"
 				+ "For further assistance, click 'Help'");
-	} 
+	}
 
 	/**
-	 * Invoked when any of the following menu action occurs: Export, Import, 
-	 * Process, Flag Guide, Clear. Interacts with the ActionListener class 
+	 * Invoked when any of the following menu action occurs: Export, Import,
+	 * Process, Flag Guide, Clear. Interacts with the ActionListener class
+	 * 
 	 * @param event the event to be processed
 	 */
-	
-	public void actionPerformed(ActionEvent event) 
-	{ 
-		String select = event.getActionCommand(); 
 
-		if (select.equals("Export")) 
-		{ 
+	public void actionPerformed(ActionEvent event) {
+		String select = event.getActionCommand();
 
-			//Create file selection object 
-			JFileChooser selectExp = new JFileChooser("f:"); 
+		if (select.equals("Export")) {
 
-			int saveE = selectExp.showSaveDialog(null); 
+			// Create file selection object
+			JFileChooser selectExp = new JFileChooser("f:");
 
-			if (saveE == JFileChooser.APPROVE_OPTION) 
-			{ 
+			int saveE = selectExp.showSaveDialog(null);
 
-				File label = new File(selectExp.getSelectedFile().
-						getAbsolutePath()); 
+			if (saveE == JFileChooser.APPROVE_OPTION) {
 
-				try 
-				{ 
-					//write files 
-					FileWriter writer = new FileWriter(label, false); 
-					BufferedWriter wfile = new BufferedWriter(writer); 
+				File label = new File(selectExp.getSelectedFile().getAbsolutePath());
 
-					wfile.write(bottomText.getText()); 
-					wfile.flush(); 
-					wfile.close(); 
-				} 
-				catch (Exception evt) 
-				{ 
-					JOptionPane.showMessageDialog(frameOne, evt.getMessage()); 
-				} 			
-			} 
-			else
-				//cancel option text 
-				JOptionPane.showMessageDialog(frameOne, 
-						"A file was not exported"); 
-		} 
-		else if (select.equals("Import")) 
-		{ 
-			JFileChooser selectImp = new JFileChooser("f:"); 
+				try {
+					// write files
+					FileWriter writer = new FileWriter(label, false);
+					BufferedWriter wfile = new BufferedWriter(writer);
 
-			int saveI = selectImp.showOpenDialog(null); 
+					wfile.write(bottomText.getText());
+					wfile.flush();
+					wfile.close();
+				} catch (Exception evt) {
+					JOptionPane.showMessageDialog(frameOne, evt.getMessage());
+				}
+			} else
+				// cancel option text
+				JOptionPane.showMessageDialog(frameOne, "A file was not exported");
+		} else if (select.equals("Import")) {
+			JFileChooser selectImp = new JFileChooser("f:");
 
-			//if a file is selected 
-			if (saveI == JFileChooser.APPROVE_OPTION) 
-			{ 	
-				//obtain file path 
-				File fileIn = new File(selectImp.getSelectedFile().
-						getAbsolutePath()); 
+			int saveI = selectImp.showOpenDialog(null);
 
-				try { 
-					FileReader fRead = new FileReader(fileIn); 
-					BufferedReader bRead = new BufferedReader(fRead); 
+			// if a file is selected
+			if (saveI == JFileChooser.APPROVE_OPTION) {
+				// obtain file path
+				File fileIn = new File(selectImp.getSelectedFile().getAbsolutePath());
 
-					//obtain file name input 
-					textStr = bRead.readLine(); 
+				try {
+					FileReader fRead = new FileReader(fileIn);
+					BufferedReader bRead = new BufferedReader(fRead);
+
+					// obtain file name input
+					textStr = bRead.readLine();
 					String str1 = "";
 
-					while ((str1 = bRead.readLine()) != null) 
-					{ 
-						textStr = textStr + "\n" + str1; 
-					} 
-					//display top text
-					topText.setText(textStr); 
+					while ((str1 = bRead.readLine()) != null) {
+						textStr = textStr + "\n" + str1;
+					}
+					// display top text
+					topText.setText(textStr);
 					fRead.close();
 					bRead.close();
-				} 
-				catch (Exception evt) 
-				{ 
-					JOptionPane.showMessageDialog(frameOne, evt.getMessage()); 
-				} 
-				imported = true; 
-				
-			} 
-			else
-				JOptionPane.showMessageDialog(frameOne, 
-						"A file was not imported"); 
-		} 
-		else if (select.equals("Process")) 
-		{ 
-				//if a file is selected 
-				if (imported == true) 
-				{ 	
-					//formats the output text and displays it in bottom window
-					TextModifier modedText = new TextModifier(textStr);
-					modedText.formatString(textStr);
-					bottomText.setText(modedText.getFormattedString()); 
-					JOptionPane.showMessageDialog(frameOne,"File Processed"); 
-				} 
-				else
-				{
-					JOptionPane.showMessageDialog(frameOne, 
-							"Please import a file to process"); 
-				} 		
-		}
-		else if (select.equals("Clear")) 
-		{ 
-			topText.setText(""); 
-			bottomText.setText(""); 
+				} catch (Exception evt) {
+					JOptionPane.showMessageDialog(frameOne, evt.getMessage());
+				}
+				imported = true;
+
+			} else
+				JOptionPane.showMessageDialog(frameOne, "A file was not imported");
+		} else if (select.equals("Process")) {
+			// if a file is selected
+			if (imported == true) {
+				// formats the output text and displays it in bottom window
+				TextModifier modedText = new TextModifier(textStr);
+				modedText.formatString(textStr);
+				bottomText.setText(modedText.getFormattedString());
+				JOptionPane.showMessageDialog(frameOne, "File Processed");
+			} else {
+				JOptionPane.showMessageDialog(frameOne, "Please import a file to process");
+			}
+		} else if (select.equals("Clear")) {
+			topText.setText("");
+			bottomText.setText("");
 			imported = false;
 			textStr = "";
-		} 
-		else if (select.equals("Flag Guide")) 
-		{ 
-			JOptionPane.showMessageDialog(frameOne, "Flag Instructions\n" +
-					"'-c' center text");
+		} else if (select.equals("Flag Guide")) {
+			JOptionPane.showMessageDialog(frameOne, "Flag Instructions\n" + "'-c' center text");
 		}
-	} 
-} 
+	}
+}
--- TextEditor/src/project360/TextModifier.java
+++ TextEditor/src/project360/TextModifier.java
@@ -4,41 +4,55 @@ import java.util.*;
 import java.util.LinkedList; 
 import java.util.Queue; 
 
+/**
+ * @author Azaria Fowler
+ * @author Taylor Gordon
+ * @author Laura Karp
+ * @author Liang Wang
+ * 
+ *  Class ID: 70642 - CSE 360 - Wednesday Team Number: 39 Group Project
+ * 
+ *  360 Text Editor - A program that creates a formatted text file given a 
+ *  document with user created formatting flags. 
+ * 
+ * @version 1.0 November 22, 2019
+ **/
+
 
 public class TextModifier 
 {
 	private String rawText;
 	private String formattedString;
-	
+
 	private Queue<Character> commandQueue;
-	
-	
+
+
 	TextModifier (String text) {
 		rawText = text;
 		formattedString = "";
 		commandQueue = new LinkedList<>();
 	}
-	
+
 	// Methods based on command.
 	String returnSpaces(int count) {
 		String spaces = "";
-		
+
 		for (int i = 0; i < count; i++) {
 			spaces += " ";
 		}
-		
+
 		return spaces;
 	}
-	
+
 	String leftText(String line) {
 		line = line.trim().replaceAll("\\s+", " ");
-		
+
 		if (line.length() > 80) {
 			String newLine = line.substring(0, 80);
-			
+
 			String[] words = newLine.split(" ");
 			String lastWord = words[words.length-1];
-			
+
 			if (newLine.charAt(79) != lastWord.charAt(0) || newLine.charAt(79) != ' ') {
 				// The last word is cut off!
 				newLine = "";
@@ -47,10 +61,10 @@ public class TextModifier
 				}
 				newLine = newLine.substring(0, newLine.length()-1);
 			}
-			
+
 			newLine = leftText(newLine);
 			String nextLine = leftText(line.substring(80 - lastWord.length() -1));
-			
+
 			return newLine + nextLine;
 		} else {
 			int numOfSpaces = 80 - line.length();
@@ -59,16 +73,16 @@ public class TextModifier
 			return newLine + "\n";
 		}
 	}
-	
+
 	String rightText(String line) {
 		line = line.trim().replaceAll("\\s+", " ");
-		
+
 		if (line.length() > 80) {
 			String newLine = line.substring(0, 80);
-			
+
 			String[] words = newLine.split(" ");
 			String lastWord = words[words.length-1];
-			
+
 			if (newLine.charAt(79) != lastWord.charAt(0) || newLine.charAt(79) != ' ') {
 				// The last word is cut off!
 				newLine = "";
@@ -77,10 +91,10 @@ public class TextModifier
 				}
 				newLine = newLine.substring(0, newLine.length()-1);
 			}
-			
+
 			newLine = rightText(newLine);
 			String nextLine = rightText(line.substring(80 - lastWord.length() -1));
-			
+
 			return newLine + nextLine;
 		} else {
 			int numOfSpaces = 80 - line.length();
@@ -89,16 +103,16 @@ public class TextModifier
 			return newLine + "\n";
 		}
 	}
-	
+
 	String centerText(String line) {
 		line = line.trim().replaceAll("\\s+", " ");
 		if (line.length() > 80) {
-			
+
 			String newLine = line.substring(0, 80);
-			
+
 			String[] words = newLine.split(" ");
 			String lastWord = words[words.length-1];
-			
+
 			if (newLine.charAt(79) != lastWord.charAt(0) || newLine.charAt(79) != ' ') {
 				// The last word is cut off!
 				newLine = "";
@@ -107,25 +121,25 @@ public class TextModifier
 				}
 				newLine = newLine.substring(0, newLine.length()-1);
 			}
-			
+
 			newLine = centerText(newLine);
 			String nextLine = centerText(line.substring(80 - lastWord.length() -1));
-			
+
 			return newLine + nextLine;
 		} else {	
 			String[] words = line.split(" ");
 			String lastWord = words[words.length-1];
-			
+
 			int numOfSpaces = 80 - line.length();
 			int spacesPerWord = numOfSpaces/(words.length-1) + 1;
 			int remainingSpaces = line.length() + spacesPerWord*(words.length-1) - words.length;
-			
+
 			if (remainingSpaces < 80) {
 				remainingSpaces = 80 - remainingSpaces - 1;
 			} else {
 				remainingSpaces = 0;
 			}
-				
+
 			String newLine = "";
 
 			for (int i = 0; i < words.length-1; i++) {
@@ -135,23 +149,23 @@ public class TextModifier
 				}
 			}
 			newLine += lastWord;
-			
+
 			System.out.println(newLine.length());
-			
+
 			return newLine + "\n";
 		}
 	}
-	
+
 	String titleText(String line) {
 		line = line.trim().replaceAll("\\s+", " ");
-		
+
 		if (line.length() > 80) {
-			
+
 			String newLine = line.substring(0, 80);
-			
+
 			String[] words = newLine.split(" ");
 			String lastWord = words[words.length-1];
-			
+
 			if (newLine.charAt(79) != lastWord.charAt(0) || newLine.charAt(79) != ' ') {
 				// The last word is cut off!
 				newLine = "";
@@ -160,33 +174,33 @@ public class TextModifier
 				}
 				newLine = newLine.substring(0, newLine.length()-1);
 			}
-			
+
 			newLine = titleText(newLine);
 			String nextLine = titleText(line.substring(80 - lastWord.length() -1));
-			
+
 			return newLine + nextLine;
 		} else {
 			int numOfSpaces = 80 - line.length();
 			String newLine = returnSpaces(numOfSpaces/2);
 			newLine += line;
 			newLine += returnSpaces(numOfSpaces/2);
-			
+
 			if (newLine.length() < 80) {
 				newLine += " ";
 			}
-			
+
 			return newLine + "\n";
-	
+
 		}
 	}
-	
+
 	String doubleText(String line) {
 		if (line.length() > 80) {
 			String newLine = line.substring(0, 80);
-			
+
 			String[] words = newLine.split(" ");
 			String lastWord = words[words.length-1];
-			
+
 			if (newLine.charAt(79) != lastWord.charAt(0) || newLine.charAt(79) != ' ') {
 				// The last word is cut off!
 				newLine = "";
@@ -195,23 +209,23 @@ public class TextModifier
 				}
 				newLine = newLine.substring(0, newLine.length()-1);
 			}
-			
+
 			newLine = doubleText(newLine);
 			String nextLine = doubleText(line.substring(80 - lastWord.length() -1));
-			
+
 			return newLine + "\n" + nextLine;
 		} else {
 			return line + "\n";
 		}
 	}
-	
+
 	String singleText(String line) {
 		if (line.length() > 80) {
 			String newLine = line.substring(0, 80);
-			
+
 			String[] words = newLine.split(" ");
 			String lastWord = words[words.length-1];
-			
+
 			if (newLine.charAt(79) != lastWord.charAt(0) || newLine.charAt(79) != ' ') {
 				// The last word is cut off!
 				newLine = "";
@@ -220,32 +234,61 @@ public class TextModifier
 				}
 				newLine = newLine.substring(0, newLine.length()-1);
 			}
-			
+
 			newLine = doubleText(newLine);
 			String nextLine = doubleText(line.substring(80 - lastWord.length() -1));
-			
+
 			return newLine + nextLine;
 		} else {
 			return line + "\n";
 		}
 	}
 	
+	String indentText(String line) {
+
+		if (line.length() > 75) {
+			int split = 75;
+			char spltWords = line.charAt(split);
+
+			//prevent mid word splitting 
+			if (spltWords != ' '){
+				while (spltWords != ' ' && spltWords != -1){
+					--split;			
+					spltWords = line.charAt(split);
+
+				}
+			}
+
+			String iLine = line.substring(0, split); //line to be indented 
+
+			iLine = returnSpaces(5) + iLine;
+			line = leftText(line.substring(split + 1));
+			line = iLine + "\n" + line;
+		}
+		else
+		{
+			line = returnSpaces(5) + line;
+		}
+		
+		return line; 
+	}
+	
 	String envokeCommand(char command, String line) {
 		switch (command) {
-		case 'l':
+		case 'l': //left justified
 			return leftText(line);
-		case 'r':
+		case 'r': //right justified
 			return rightText(line);
-		case 'c':
+		case 'c': //justified 
 			return centerText(line);
-		case 't':
+		case 't': //title
 			return titleText(line);
-		case 'd':
+		case 'd': //double spaced lines
 			return doubleText(line);
-		case 's':
+		case 's': //single spaced lines
 			return singleText(line);
-		case 'i':
-			return line;
+		case 'i': //indented first line
+			return indentText(line);
 		case 'b':
 			return line;
 		case '2':
@@ -258,11 +301,15 @@ public class TextModifier
 			return line;
 		}
 	}
-	
+
 	void formatString(String text) {
 		// Logic on parsing the rawText using the methods above.
 		String lines[] = rawText.split("\\r?\\n"); // Split the rawText into lines
-		
+
+		//commandQueue.add('l'); //left justified default
+		//commandQueue.add('s'); //single space default
+		//commandQueue.add('1'); //one column default
+
 		for (int i = 0; i < lines.length; i++) {
 			if (lines[i].charAt(0) == '-') { // We are reading a command
 				// Add to the commandQueue
@@ -278,7 +325,7 @@ public class TextModifier
 			}
 		}
 	}
-	
+
 	String getFormattedString() {
 		return formattedString;
 	}
